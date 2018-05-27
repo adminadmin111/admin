5d83e53caf5ba5657ed294739e5e6d304bb1c3f6
adminadmin111 <812519112@qq.com>
2018-05-27 20:13:13 +0800

    gengxinneirong

--- scripts/test001.py
+++ scripts/test001.py
@@ -3,7 +3,7 @@ import pytest
 
 class Test_001:
     @allure.step(title="测试步骤001")
-    @pytest.mark.parametrize("a",[1,2,3])
+    @pytest.mark.parametrize("a",[1,5,3])
     def test_001(self, a):
         assert a!= 2
     @allure.step(title="测试步骤002")
